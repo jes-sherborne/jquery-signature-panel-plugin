1ead7ecd3bb87f3553cf486940a7f6dbca38a897
Jes Sherborne <jes.sherborne@gmail.com>
2010-11-28 22:55:52 -0600

    Code basically works in Chrome, Safari, and Firefox

--- README.markdown
+++ README.markdown
@@ -1,5 +1,5 @@
 A jquery-based panel to capture signatures. Compatible with iPad.
 
 * Captures all signature data (location and timing), which provides a stronger case when authenticating the signature
-* performs modest input smoothing so that so that captured signature more closely matches the user's intent
+* performs modest input smoothing so that so that the captured signature more closely matches the user's intent
 * Uses spline interpolation for drawing so that signature looks realistic
--- signature-panel.js
+++ signature-panel.js
@@ -1,1 +1,169 @@
+(function($) {
 
+	var settings = {
+		"penColor": "black",
+		"penWidth": 3.0,
+		"clearCaption": "Clear",
+		"okCaption": "OK",
+		"cancelCaption": "Cancel",
+		"drawPrompt": "Sign here using your mouse or touchscreen",
+		"backColor" : "white",
+		"borderColor": "gray"
+	};
+
+	var state = {
+		x : 0,
+		y : 0,
+		drawing : false,
+		startTime : 0
+	};
+
+	var internal = {
+		getPanelHtml : function ($parentDiv){
+			var r = [];
+
+			r.push("<canvas height=\"" + ($parentDiv.height() - 40) + "px\" width=\"" + ($parentDiv.width() + 0.0) + "px\"></canvas>");
+			r.push("<div style=\"height: 40px; padding: 2px 6px 2px 6px;\">");
+			r.push("<a href=\"#\" class=\"signature-panel-clear\">Clear</a>");
+			r.push("</div>");
+			return r.join("\n");
+		},
+		clearCanvas : function ($canvas, context) {
+			var canvas = $canvas[0];
+
+			context.save();
+			context.closePath();
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.restore();
+
+		}
+	};
+
+	var methods = {
+		init : function(options) {
+
+			return this.each(function() {
+				var $this, data, $canvas, context;
+
+				$this = $(this);
+
+				// Attach data storage to this object
+				if (! $this.data("signaturePanel")) {
+					$this.data("signaturePanel", {
+						clickstream : []
+					});
+				}
+				data = $this.data("signaturePanel");
+
+				// Apply user-supplied options
+				if (options) {
+					$.extend(settings, options);
+				}
+
+				// Create user interface elements
+				$this.empty();
+				$this.append(internal.getPanelHtml($this));
+
+				$canvas = $this.find("canvas");
+				context = $canvas[0].getContext('2d');
+
+				context.lineWidth = settings.penWidth;
+				context.strokeStyle = settings.penColor;
+				context.fillStyle = "none";
+
+				// Attach event handlers
+
+				$this.find("a.signature-panel-clear").bind("click.signaturePanel", function () {
+					internal.clearCanvas($canvas, context);
+				});
+
+				$canvas.bind("mousedown.signaturePanel", function (event) {
+					state.startTime = (new Date).getTime();
+					state.x = event.offsetX;
+					state.y = event.offsetY;
+					state.drawing = true;
+					context.beginPath();
+					context.moveTo(event.offsetX, event.offsetY);
+					data.clickstream.push({x: state.x, y: state.y, t: 0, action: "mousedown"});
+					event.preventDefault;
+				});
+
+				$(document).bind("mousemove.signaturePanel", function (event) {
+					var x, y, t;
+					if (state.drawing) {
+						t= (new Date).getTime - state.startTime;
+						x = event.pageX - $canvas[0].offsetLeft;
+						y = event.pageY - $canvas[0].offsetTop;
+						context.lineTo(x, y);
+						context.stroke();
+						state.x = x;
+						state.y = y;
+						data.clickstream.push({x: state.x, y: state.y, t: t, action: "mousemove"});
+						event.preventDefault;
+					}
+				});
+
+				$(document).bind("mouseup.signaturePanel", function (event) {
+					var x, y, t;
+					if (state.drawing) {
+						t = (new Date).getTime - state.startTime;
+						x = event.pageX - $canvas[0].offsetLeft;
+						y = event.pageY - $canvas[0].offsetTop;
+						context.lineTo(x, y);
+						context.stroke();
+						context.closePath();
+						state.x = x;
+						state.y = y;
+						state.drawing = false;
+						data.clickstream.push({x: state.x, y: state.y, t: t, action: "mouseup"});
+						event.preventDefault;
+					}
+				});
+
+			});
+		},
+
+		destroy : function() {
+
+			return this.each(function() {
+				var $this, data, $canvas;
+
+				$this = $(this);
+				data = $this.data("signaturePanel");
+				$canvas = $this.find("canvas");
+
+				$canvas.unbind(".signaturePanel");
+				$(document).unbind(".signaturePanel");
+
+				data.signaturePanel.remove();
+				$this.removeData("signaturePanel");
+			})
+		},
+
+		clear : function() {
+			return this.each(function() {
+				var $canvas, context;
+
+				$canvas = $this.find("canvas");
+				context = $canvas[0].getContext('2d');
+
+				internal.clearCanvas($canvas, context);
+
+			})
+		}
+
+	};
+
+	$.fn.signaturePanel = function(method) {
+
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === "object" || ! method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error("Method " + method + " does not exist on jQuery.signaturePanel");
+		}
+
+	};
+
+})(jQuery);
